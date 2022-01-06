964f626a20dfd4cde531840dfdeb94993cab190f
Bodemar <shaydon@88665a406f8a.ant.amazon.com>
2022-01-05 19:54:14 -0800

    Add setup script and README
    Problem: There was not yet a way for the configuration files to be easily integrated with the local environment from this repository. There also was not yet a README describing what this repository is.
    
    Solution: Add a script that, although does not have any features in place yet, describes many of the ideated features in order to stay organized when working on this project in the future.
    
    Testing: None necessary, no code yet in the script and just markdown for the README.md
    
    Signed-off-by: shaydonb

---
+++ README.md
@@ -0,0 +1,6 @@
+# Personal Configuration Files
+## Background
+This project was started since there will be no shortage of use of these tools, so it would be beneficial to have some manner of easily maintaining configuration files for commonly used tools.
+
+## Features
+Ideated features currently reside in `TODO` file items within the `config-setup.sh` file.
---
+++ config-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script sets up the configuration files that will be used by ther respective tools.
+# TODO: Write the script to emplace/replace the configuration files.
+# TODO: Add features to prevent adding configuration files for packages not present/installed.
+# TODO: Add a (flag-prompted?) feature to install certain missing packages.
+# TODO: Ask the user if they would like to install each package not yet present.
+# TODO: Allow user to run this script even with slight changes tho the configs.
+    # TODO: Enable the above feature to only add lines in the git-maintained
+    #       config that are missing in the local config, rather than purely
+    #       overwriting, in the event that any local changes were made that
+    #       aren't desired on all machines.
+
